f9079935d7cacb1389e1a314fe1718520f8f26c0
Grira <farouk.grira@sesame.com.tn>
2020-01-17 11:39:30 +0100

    other testing e2e checking on travis

--- protractor.ci.conf.js
+++ protractor.ci.conf.js
@@ -3,7 +3,7 @@ const config = require('./e2e/protractor.conf').config;
 config.capabilities = {
     browserName: 'chrome',
     chromeOptions: {
-        args: ['--headless', '--no-sandbox']
+        args: ['--headless']
     }
 };
 
